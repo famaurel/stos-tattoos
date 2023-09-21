f965a0195ab9e51d69597947d4d799dd46d0d2fc
Fanny Maurel <fannymaurel@MBP-de-Fanny.lan>
2023-09-21 17:57:05 +0200

    basic pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "pages#home"
+  get "about", to: "pages#about"
+  get "work", to: "pages#work"
+  get "contact", to: "pages#contact"
 end
